8d4172c0a6874022516b1a74ece000da975366ca
Emanuel TSR <nunesalmeidae@gmail.com>
2025-11-10 10:01:00 -0300

    Banco de dados, projeto maven, repositorios e inicio dos controlers

---
+++ src/main/java/br/com/iesb/guia_iesb/GuiaIesbApplication.java
@@ -0,0 +1,13 @@
+package br.com.iesb.guia_iesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaIesbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaIesbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iesb/guia_iesb/GuiaIesbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iesb.guia_iesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaIesbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
